3084ca1c36fc80b6d76a0a1f2804ca789a549dae
Viktor1732 <programmer.kras@gmail.com>
2023-04-07 21:33:44 +0700

    Прописал примерную работу клиентской части, ответы на команды. Добавлена функция on_startup, позволяющая увидеть служебную информацию, при запуске бота.

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,14 +1,40 @@
+import os
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from dotenv import load_dotenv, find_dotenv
 
-import os
-
 load_dotenv(find_dotenv())
 
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot)
+
+
+async def on_startup(_):
+    print('Бот вышел в онлаин!')
+
+
+""" ************** КЛИЕНТСКАЯ ЧАСТЬ **************"""
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def command_star(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await message.delete()
+    except:
+        await message.reply("Общение с ботом через лс, напишите ему: \nhttps://t.me/Pizza_SheefOkBot")
+
+
+@dp.message_handler(commands=['Время_работы'])
+async def pizza_open_command(message: types.Message):
+    await bot.send_message(message.from_user.id, "Работаем без выходных, часы работы: 7:00-21:00")
+
+
+@dp.message_handler(commands=['Адрес'])
+async def pizza_adress_command(message: types.Message):
+    await bot.send_message(message.from_user.id, "Адрес:  г.Красноярск, ул.Ленина, д.287, пом.1")
 
 
 @dp.message_handler()
@@ -26,4 +52,4 @@ async def echo_send(message: types.Message):
     # await bot.send_message(message.from_user.id, message.text)
 
 
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
