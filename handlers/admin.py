6cb05743df9ad0372feb27e49c0fbfbe5c501ca3
Viktor1732 <programmer.kras@gmail.com>
2023-04-09 23:38:29 +0700

    Прописал проверку, на администратора группы, ограничил доступ к загрузке данных для простых пользователей.

--- handlers/admin.py
+++ handlers/admin.py
@@ -3,6 +3,10 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
+from create_bot import bot
+
+ID = None
+
 
 class FSMAdmin(StatesGroup):
     photo = State()
@@ -11,54 +15,68 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 
+# Получаем ID текущего модератора
+async def make_changes_command(message: types.Message):
+    global ID
+    ID = message.from_user.id
+    await bot.send_message(message.from_user.id, 'Чего хототите хозяин?')
+    await message.delete()
+
+
 # Начало диалога нового пункта меню
 async def cm_start(message: types.Message):
-    await FSMAdmin.photo.set()
-    await message.reply('Загрузить фото')
+    if message.from_user.id == ID:
+        await FSMAdmin.photo.set()
+        await message.reply('Загрузить фото')
 
 
 # Ловим первый ответ от пользователя
 async def load_photo(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['photo'] = message.photo[0].file_id
-        await FSMAdmin.next()
-        await message.reply('Введите название')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['photo'] = message.photo[0].file_id
+            await FSMAdmin.next()
+            await message.reply('Введите название')
 
 
 # Ловим второй ответ от пользователя
 async def load_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = message.text
-        await FSMAdmin.next()
-        await message.reply('Введите описание')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text
+            await FSMAdmin.next()
+            await message.reply('Введите описание')
 
 
 # Ловим третий ответ от пользователя
 async def load_description(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['description'] = message.text
-        await FSMAdmin.next()
-        await message.reply('Укажите цену')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['description'] = message.text
+            await FSMAdmin.next()
+            await message.reply('Укажите цену')
 
 
 # Ловим четвертый ответ и используем полученные данные
 async def load_price(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['price'] = round(float(message.text), 2)
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = round(float(message.text), 2)
 
-    async with state.proxy() as data:
-        await message.reply(str(data))
+        async with state.proxy() as data:
+            await message.reply(str(data))
 
-    await state.finish()
+        await state.finish()
 
 
 # Выход из состояний
 async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await message.reply('Вы отменили загрузка данных!')
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply('Вы отменили загрузка данных!')
 
 
 # Регистрируем хендлеры
@@ -70,3 +88,4 @@ def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(load_price, state=FSMAdmin.price)
     dp.register_message_handler(cancel_handler, state="*", commands='Отмена')
     dp.register_message_handler(cancel_handler, Text(equals='Отмена', ignore_case=True), state="*")
+    dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
