fc9d79a6ca8d28ff4018298f36c4b8c955a9e6ab
Viktor1732 <programmer.kras@gmail.com>
2023-04-09 22:52:33 +0700

    Прописал машину состояний c загрузкой данных: фото, название, описание, цена.

--- create_bot.py
+++ create_bot.py
@@ -1,10 +1,13 @@
 import os
 
 from aiogram import Bot
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 from dotenv import load_dotenv, find_dotenv
+
+storage = MemoryStorage()
 
 load_dotenv(find_dotenv())
 
 bot = Bot(os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
---
+++ handlers/admin.py
@@ -0,0 +1,60 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class FSMAdmin(StatesGroup):
+    photo = State()
+    name = State()
+    description = State()
+    price = State()
+
+
+# Начало диалога нового пункта меню
+async def cm_start(message: types.Message):
+    await FSMAdmin.photo.set()
+    await message.reply('Загрузить фото')
+
+
+# Ловим первый ответ от пользователя
+async def load_photo(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['photo'] = message.photo[0].file_id
+        await FSMAdmin.next()
+        await message.reply('Введите название')
+
+
+# Ловим второй ответ от пользователя
+async def load_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['name'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Введите описание')
+
+
+# Ловим третий ответ от пользователя
+async def load_description(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['description'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Укажите цену')
+
+
+# Ловим четвертый ответ и используем полученные данные
+async def load_price(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['price'] = round(float(message.text), 2)
+
+    async with state.proxy() as data:
+        await message.reply(str(data))
+
+    await state.finish()
+
+
+# Регистрируем хендлеры
+def register_handlers_admin(dp: Dispatcher):
+    dp.register_message_handler(cm_start, commands=['Загрузить'], state=None)
+    dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
+    dp.register_message_handler(load_name, state=FSMAdmin.name)
+    dp.register_message_handler(load_description, state=FSMAdmin.description)
+    dp.register_message_handler(load_price, state=FSMAdmin.price)
