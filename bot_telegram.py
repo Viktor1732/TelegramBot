a96a07029c7a40cc5be0dd81cdb842c25ea3d6b5
Viktor1732 <99543565+Viktor1732@users.noreply.github.com>
2023-04-12 16:15:11 +0700

    Прописал удаление данных из БД для администратора.

--- bot_telegram.py
+++ bot_telegram.py
@@ -15,5 +15,4 @@ client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
-
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
--- handlers/admin.py
+++ handlers/admin.py
@@ -2,8 +2,9 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from create_bot import bot
+from create_bot import bot, dp
 from data_base import sqlite_db
 from key_boards import admin_kb
 
@@ -79,6 +80,21 @@ async def load_price(message: types.Message, state: FSMContext):
         await state.finish()
 
 
+@dp.callback_query_handler(lambda x: x.data and x.data.startswith("del "))
+async def del_callback_run(callback_query: types.CallbackQuery):
+    await sqlite_db.sql_delete_command(callback_query.data.replace('del ', ''))
+    await callback_query.answer(text=f'{callback_query.data.replace("del ", "")} удалена.', show_alert=True)
+
+
+async def delete_item(message: types.Message):
+    if message.from_user.id == ID:
+        read = await sqlite_db.sql_read2()
+        for ret in read:
+            await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена: {ret[-1]}')
+            await bot.send_message(message.from_user.id, text='^^^', reply_markup=InlineKeyboardMarkup().add(
+                InlineKeyboardButton(f'Удалить {ret[1]}', callback_data=f'del {ret[1]}')))
+
+
 # Регистрируем хендлеры
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(cancel_handler, state="*", commands='Отмена')
@@ -89,3 +105,4 @@ def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
     dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
+    dp.register_message_handler(delete_item, commands=['Удалить'])
